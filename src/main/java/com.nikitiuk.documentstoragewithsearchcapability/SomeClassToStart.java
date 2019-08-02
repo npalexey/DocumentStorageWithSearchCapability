b915b6e49f31e8a43c78fe8c2bfe2b28fed337fd
npalexey <schierke.schierke@gmail.com>
2019-08-02 15:14:29 +0300

    NewPlace

--- src/main/java/com.nikitiuk.documentstoragewithsearchcapability/SomeClassToStart.java
+++ src/main/java/com.nikitiuk.documentstoragewithsearchcapability/SomeClassToStart.java
@@ -7,5 +7,6 @@ public class SomeClassToStart {
     private static final Logger logger =  LoggerFactory.getLogger(SomeClassToStart.class);
     public static void main(String[] args) {
         logger.info("Storage will be here soon");
+        logger.info("Something to change");
     }
 }
