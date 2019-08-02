b506ba33e94bc3c50ea8905db04a8b02324ed960
npalexey <schierke.schierke@gmail.com>
2019-08-02 14:59:22 +0300

    Initial commit

---
+++ src/main/java/com.nikitiuk.documentstoragewithsearchcapability/SomeClassToStart.java
@@ -0,0 +1,11 @@
+package com.nikitiuk.documentstoragewithsearchcapability;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SomeClassToStart {
+    private static final Logger logger =  LoggerFactory.getLogger(SomeClassToStart.class);
+    public static void main(String[] args) {
+        logger.info("Storage will be here soon");
+    }
+}
